8ecb0c3335774ab0d56413b18ca0365ba535ad7d
Ubuntu <ubuntu@ip-172-31-32-98.us-east-2.compute.internal>
2019-06-27 18:24:35 +0000

    added awesome emoticons

--- ruby_quiz.rb
+++ ruby_quiz.rb
@@ -32,7 +32,7 @@ end
 question1
 
 def question2
-    puts "TWO: How fit are you?\n1: I could climb a mountain\n2: Dost thou even hoist, brethren?\n3: Crossfit is a lifestyle\n4: I like to eat\n"
+    puts "TWO: How fit are you?\n1: I could climb a mountain\n2: Dost thou even hoist, brethren?\n3: Crossfit is a lifestyle\n4: I run... to the fridge\n"
     q2 = gets.chomp.upcase
         if (q2 == "1" or q2 == "2" or q2 == "3" or q2 == "4")
             $responses << q2
@@ -45,7 +45,7 @@ end
 question2
 
 def question3
-    puts "THREE: How do you act when upset?\n1: I'm above such `emotion`\n2: Throw\n3: Start a war\n4: Take solace in an extra treat\n"
+    puts "THREE: How do you act when upset?\n1: I'm above such `emotion`\n2: I might throw a tantrum or a chair\n3: I go to war\n4: I stay home and eat ice cream\n"
     q3 = gets.chomp.upcase
     if(q3 == "1" or  q3 == "2" or q3 == "3" or q3 == "4")
         $responses << q3
@@ -58,7 +58,7 @@ end
 question3
 
 def question4
-    puts "FOUR: How would your friends describe you?\n1: You like to sing\n2: You tend to be a hoarder\n3: You are ambitious\n4: You are short and loyal\n"
+    puts "FOUR: How would your friends describe you?\n1: You like to sing\n2: You have a lot of stuff in your house\n3: You are ambitious\n4: You are short and loyal\n"
     q4 = gets.chomp.upcase
     if(q4 == "1" or  q4 == "2" or q4 == "3" or q4 == "4")
         $responses << q4
@@ -71,7 +71,7 @@ end
 question4
 
 def question5
-    puts "FIVE: Favorite date?\n1: Sunset horseback ride\n2: Visit a brewery\n3: Play Call of Duty\n4: Brunch!!!\n"
+    puts "FIVE: What is your favorite date?\n1: Sunset horseback ride\n2: Visit a brewery\n3: Play Call of Duty\n4: Brunch!!!\n"
     q5 = gets.chomp.upcase
     if(q5 == "1" or q5 == "2" or q5 == "3" or q5 == "4")
         $responses << q5
@@ -98,7 +98,7 @@ question6
 
 
 def question7 
-    puts "SEVEN: What is your favorite animal?\n1: Horse\n2: Boar\n3: Dog\n4: Cat\n"
+    puts "SEVEN: What is your favorite animal?\n1: Unicorn\n2: Boar\n3: Dog\n4: Cat\n"
     q7 = gets.chomp.upcase
     if(q7 == "1" or q7 == "2" or q7 =="3" or q7 == "4")
     $responses << q7
@@ -124,7 +124,7 @@ end
 question8 
 
 def question9
-    puts "NINE: What is your favorite landscape?\n1: Forest\n2: Mountains\n3: Beach\n4: Meadows\n"
+    puts "NINE: What is your favorite landscape?\n1: Forests\n2: Mountains\n3: Beaches\n4: Meadows\n"
     q9 = gets.chomp.upcase
     if(q9 == "1" or q9 == "2" or q9 =="3" or q9 == "4")
     $responses << q9
@@ -145,24 +145,155 @@ count_3 = $responses.count("3")
 count_4 = $responses.count("4")
 
 # Add names
-elf_profile = "#{name}wë #{last_initial}inuviël, you are an elf! You are really competent and do everything gracefully and beautifully. You love nature, singing, and being better-looking than everyone else, but these attributes can lead you to being snobbish and too proud."
+elf_profile = "#{name}wë #{last_initial}inuviël, you are an elf! You are very competent and do everything gracefully and beautifully. You love nature, singing, and being better-looking than everyone else, but these attributes can lead you to being snobbish and arrogant."
 dwarf_profile = "#{name}ïn #{last_initial}oldbeard, you are a dwarf! You are honorable, serious, strongly opinionated and extremely confident in who you are. You love hiking and dirt. However, you are a little bit of a hoarder, and can come on a little too strong."
-human_profile = "#{name}gorn, son/daughter of #{last_initial}arathorn, you are a Middle-earth human! You are adaptable, dynamic, and able to get things done. You have a strong ambition for yourself and your friends, and are susceptible to disease and old age :("
-hobbit_profile = "#{name}bo #{last_initial}obbins, you are a hobbit! You are short, love to eat many meals and have huge, hairy feet. You are a simple, fun-loving individual. In trying circumstances, you try to be positive and prove yourself to be quite brave. You have strong ties to your family and to tradition."
+human_profile = "#{name}gorn, son/daughter of #{last_initial}arathorn, you are a Middle-earth human! You are adaptable, dynamic, and able to get things done. You have a strong drive for yourself and your friends, and are susceptible to disease and old age :("
+hobbit_profile = "#{name}bo #{last_initial}obbins, you are a hobbit! You are short, love to eat many meals and have huge, hairy feet. You are a simple, fun-loving, and loyal friend. In trying circumstances, you try to be positive and prove yourself to be quite brave. You have strong ties to your family and to tradition."
 wizard_profile = "You are a magical wizard named #{name}dalf the Chartreuse. You are beautiful, noble, adapatable, and short. You were born as an old person like Benjamin Button and that means you're wise!"
 
-# return something if there are two frontrunners
-new_frontrunner = ""
 if (count_1 > count_2 && count_1 > count_3 && count_1 > count_4) 
     puts elf_profile
+    puts "                  .;;,.
+                  ; ' ;\ \//
+                 \|o (o|7 \//
+                 j| ..  | ||/
+                //'.--.')\-,/
+              .-||- '' ||/  `-.
+             ;  | \ |/ |/ L.  ,|
+             f\ |\| Y  || \ '._|
+            j | \|     (| |   | |
+           |  L_\         L.__: |
+            \(  '-.,-,    |   ; |
+             |'-.'.L_rr>  f--f  |
+.-=,,______,--------- J-. ;  ;__ 
+   ``-,__   |  |      h  |  f  '--.__
+       `--;;--,_       h  f-j   |   __;==-.
+            / `-''-,,__J,'  \_..--:'-'     '
+            | |    `' --L7//'-'`|
+            | ,     ||  h    |  (
+            | ;     | \ J    j   |
+            | L__   | |  L_.'    |
+            |   |'-.| L.'h  |  : |
+            |;  \     |  J ; : : |
+            | :  (    \  'L| : : |
+            | ;   |'.--|    \  : |
+            | | : \    \-, /`\ : |
+            L-'-;__\   \| '  | | |
+                    ;   \|   |'L_j
+                    _>  _|   |
+             snd   <___/ /-  |
+                        /    /
+                        '---'"
 elsif (count_2 > count_1 && count_2 > count_3 && count_2 > count_4) 
     puts dwarf_profile
+    puts "             _.-;-._
+            ;_.JL___; 
+            F-/\_-7L
+            | o/ o | |
+           ,L,c;,.='/;,
+        _,-;;S:;:S;;:' '--._
+       ;. \;;s:::s;;: .'   /|
+      /  \  ;::::;;  /    /  |
+     / ,  k ;S';;'S.'    j __,l
+  ,---/| /  /S   /S '.   |'   ;
+ ,Ljjj |/|.' s .' s   \  L    |
+ LL,_ ]( \    /    '.  '.||   ;
+ ||\ > /  ;-.'_.-.___\.-'(|==(
+ JJ, /   |_  [   ]     _]|   /
+  LL\/   ,' '--'-'-----'  \  ( 
+  ||     ;      |          |  >
+  JJ     |      |\         |,|
+   LL    |      ||       ' | 
+   ||    |      ||       . |
+   JJ    /_     ||       ;_|
+    LL   L ==='|i======='_|
+    ||    i----' '-------';
+    JJ    ';-----.------,-'
+     LL     L_.__J,'---;'
+     ||      |   ,|    (
+     JJ     .'=  (|  ,_|
+      LL   /    .'L_    |
+snd   ||   '---'    '.___>"
 elsif (count_3 > count_1 && count_3 > count_2 && count_3 > count_4) 
     puts human_profile
+    puts "                    (.
+                   / |
+                  _\ /_
+        .     .  (,'v`.)  .     .
+        \)   ( )  ,' `.  ( )   (|
+         \`. / `-'     `-' \ ,'|
+          : '    _______    ' :
+          |  _,-'  ,-.  `-._  |
+          |,' ( )__`-'__( ) `.|
+          (|,-,'-._   _.-`.-.|)
+          /  /<( o)> <( o)>\  |
+          :  :     | |     :  :
+          |  |     ; :     |  |
+          |  |    (.-.)    |  |
+          |  |  ,' ___ `.  |  |
+          ;  |)/ ,'---'. \(|  :
+      _,-/   |/\(       )/\|   \-._
+_..--'.-(    |   `-'''-'   |    )-.`--.._
+         `.  ;`._________,':  ,'
+        ,' `/               \'`.
+             `------.------'          SSt
+                    '"
 elsif (count_4 > count_1 && count_4 > count_2 && count_4 > count_3) 
     puts hobbit_profile
+    puts "                       \ : /
+                    '-: __ :-'
+                    -:  )(_ :--
+                    -' |r-_i'-
+            ,sSSSSs,   (2-,7
+            sS';:'`Ss   )-j
+           ;K e (e s7  /  (
+            S, ''  SJ (  ;/
+            sL_~~_;(S_)  _7
+|,          'J)_.-' />'-' `Z
+j J         /-;-A'-'|'--'-j|
+ L L        )  |/   :    /  |
+  | |       | | |    '._.'|  L
+   | |      | | |       | \  J
+    | |    _/ | |       |  ',|
+     | L.,' | | |       |   |/
+    _;-r-<_.| \=\    __.;  _/
+      {_}  L-'  '--'   / /|
+            |   ,      |  \|
+            |   |      |   )
+            L   ;|     |   /|
+           /|    ;     |  / |
+          | |    ;     |  ) |
+         |  |    ;|    | /  |
+         | ;|    ||    | |  |
+         L-'|____||    )/   |
+             % %/ '-,- /    /
+     snd     |% |   \%/_    |
+          ___%  (   )% |'-; |
+        C;.---..'   >%,(   '
+                   /%% /
+                  Cccc'"
 else
     puts wizard_profile
+    puts "                              /|
+                            /  \|
+                           |    |
+                         --:'''':--
+                           :'_' :
+                           _:"":\___
+            ' '      ____.' :::     '._
+           . *=====<<=)           \    :
+            .  '      '-'-'\_      /'._.'
+                             \====:_ ""
+                            .'     ||
+                           :       :
+                          /   :    |
+                         :   .      '.
+         ,. _        snd :  : :      :
+      '-'    ).          :__:-:__.;--'
+    (        '  )        '-'   '-'
+ ( -   .00.   - _
+(    .'  _ )     )
+'-  ()_.\,\,   -"
 end
 
 
